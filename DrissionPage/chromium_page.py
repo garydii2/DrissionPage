505cefd78abfb10e7cf178511d377bee73baca4a
garydii2 <gary_dii2@163.com>
2025-06-14 15:27:39 +0800

    Fix alert dialog type assignment

--- DrissionPage/chromium_page.py
+++ DrissionPage/chromium_page.py
@@ -374,7 +374,7 @@ class ChromiumPage(ChromiumBase):
         """alert出现时触发的方法"""
         self._alert.activated = True
         self._alert.text = kwargs['message']
-        self._alert.type = kwargs['message']
+        self._alert.type = kwargs.get('type')
         self._alert.defaultPrompt = kwargs.get('defaultPrompt', None)
         self._alert.response_accept = None
         self._alert.response_text = None
